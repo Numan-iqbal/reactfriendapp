f12dd6fab945213b870030bd69dd73be89d03e18
Numan <Numaniqbal560381@gmail.com>
2022-12-11 00:50:15 +0500

    Updates

---
+++ precache-manifest.09d21ed61fa37a021154249435df9918.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a92ec01aecec3b389c11",
+    "url": "/Numan-iqbal/reactfriendapp.git/static/css/main.b7777856.chunk.css"
+  },
+  {
+    "revision": "a92ec01aecec3b389c11",
+    "url": "/Numan-iqbal/reactfriendapp.git/static/js/main.a92ec01a.chunk.js"
+  },
+  {
+    "revision": "84cf99745ce0980187aa",
+    "url": "/Numan-iqbal/reactfriendapp.git/static/js/1.84cf9974.chunk.js"
+  },
+  {
+    "revision": "f56cd3ba58c833003847",
+    "url": "/Numan-iqbal/reactfriendapp.git/static/css/2.4103256c.chunk.css"
+  },
+  {
+    "revision": "f56cd3ba58c833003847",
+    "url": "/Numan-iqbal/reactfriendapp.git/static/js/2.f56cd3ba.chunk.js"
+  },
+  {
+    "revision": "052511b0ffe7021276e9",
+    "url": "/Numan-iqbal/reactfriendapp.git/static/js/runtime~main.052511b0.js"
+  },
+  {
+    "revision": "86c545ccae4a76a95a9ffdd312221e69",
+    "url": "/Numan-iqbal/reactfriendapp.git/static/media/MR ROBOT.86c545cc.woff"
+  },
+  {
+    "revision": "79401d9b9f29e19897fbbfbb3f272f59",
+    "url": "/Numan-iqbal/reactfriendapp.git/index.html"
+  }
+];
